fed2746c7107ce3c479c3a229e1a5a35a9b6e5f7
JacobLinCool <jacoblincool@gmail.com>
2023-05-16 01:08:04 +0800

    feat: change default port to 32000

--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,12 @@
 use std::net::Ipv4Addr;
 use std::path::PathBuf;
 
+use rocket::serde::{json::Json, Deserialize, Serialize};
 use rocket::Build;
 use rocket::Config;
 use rocket::Rocket;
 
+use crate::cli::build;
 use crate::compile;
 use crate::jwt;
 use crate::system;
@@ -14,11 +16,34 @@ fn index() -> &'static str {
     "I am Compilet."
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct ServerInfo {
+    pub version: String,
+    pub commit: String,
+    pub build_time: String,
+    pub os: String,
+}
+
+#[get("/info")]
+fn info() -> Json<ServerInfo> {
+    Json(ServerInfo {
+        version: build::PKG_VERSION.to_string(),
+        commit: build::COMMIT_HASH.to_string(),
+        build_time: build::BUILD_TIME.to_string(),
+        os: build::BUILD_OS.to_string(),
+    })
+}
+
 /// Get the Rocket instance
 pub fn rocket() -> Rocket<Build> {
     rocket::build()
         .configure(Config {
             address: Ipv4Addr::new(0, 0, 0, 0).into(),
+            port: std::env::var("PORT")
+                .unwrap_or_else(|_| "32000".to_string())
+                .parse()
+                .unwrap(),
             ..Config::default()
         })
         .manage(compile::WasmCache {
@@ -26,6 +51,6 @@ pub fn rocket() -> Rocket<Build> {
         })
         .mount(
             "/",
-            routes![index, system::system, compile::compile, jwt::validate],
+            routes![index, info, system::system, compile::compile, jwt::validate],
         )
 }
